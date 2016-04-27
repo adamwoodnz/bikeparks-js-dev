9e8b81bb6c92277a7a833a66663699462efdae02
Adam Wood <info@adamwood.co.nz>
2016-04-27 21:02:34 +1200

    Install react and configure babel

--- gulp/config/scripts.js
+++ gulp/config/scripts.js
@@ -4,4 +4,28 @@
  * Config
  *
  */
-module.exports = {};
+ module.exports = {
+ 	options: {
+ 		webpack: {
+ 			defaults: {
+ 				module: {
+ 					loaders: [
+ 						{
+ 							test: /\.jsx?$/,
+ 							exclude: [
+ 								/node_modules/,
+ 								/bower_components/,
+ 								/polyfills/
+ 							],
+ 							loader: 'babel',
+ 							query: {
+ 								presets: ['es2015', 'stage-2', 'react'],
+ 								plugins: ['transform-runtime']
+ 							}
+ 						}
+ 					]
+ 				}
+ 			}
+ 		}
+ 	}
+ };
